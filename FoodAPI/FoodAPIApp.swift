ee0414d91a523c8a95009499e0d4e0d313782e24
raitayami <66219215+raitayami@users.noreply.github.com>
2023-12-07 10:40:48 +0000

    Initial Commit

---
+++ FoodAPI/FoodAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAPIApp.swift
+//  FoodAPI
+//
+//  Created by Tayami Rai on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
